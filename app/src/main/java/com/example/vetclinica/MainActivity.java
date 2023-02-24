7d9b7c026701860ba0674bbfb24d3029d41b18c1
Polina <poly.gera777@gmail.com>
2023-02-24 22:19:30 +0300

    Part 4

--- app/src/main/java/com/example/vetclinica/MainActivity.java
+++ app/src/main/java/com/example/vetclinica/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.vetclinica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEnter = findViewById(R.id.btnreg);
+        btnEnter.setOnClickListener(ClickLog);
+    }
+    View.OnClickListener ClickLog = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.i(TAG, "You clicked the button!");
+        }
+    };
+    public void GoLog(View V) {
+        Log.i(TAG, "You clicked the button!");
     }
 }
