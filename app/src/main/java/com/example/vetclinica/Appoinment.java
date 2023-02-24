2bec55395afb0ae68d6f33a21592b6237fd67048
Polina <poly.gera777@gmail.com>
2023-02-24 23:45:50 +0300

    Part 6

--- app/src/main/java/com/example/vetclinica/Appoinment.java
+++ app/src/main/java/com/example/vetclinica/Appoinment.java
@@ -2,7 +2,12 @@ package com.example.vetclinica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Appoinment extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class Appoinment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinment);
+
+        Button btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(ClickBack);
     }
+
+    View.OnClickListener ClickBack = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(Appoinment.this, MainActivity.class);
+            EditText etNamePet = findViewById(R.id.etPetName);
+            String PetName = etNamePet.getText().toString();
+            intent.putExtra("namePet", PetName);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/vetclinica/MainActivity.java
+++ app/src/main/java/com/example/vetclinica/MainActivity.java
@@ -2,27 +2,40 @@ package com.example.vetclinica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnEnter = findViewById(R.id.btnreg);
+        Button btnEnter = (Button) findViewById(R.id.btnreg);
         btnEnter.setOnClickListener(ClickLog);
+
+        textView = (TextView) findViewById(R.id.fb1);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("namePet");
+        textView.setText(name);
     }
     View.OnClickListener ClickLog = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Log.i(TAG, "Программно");
+            Intent intent = new Intent(MainActivity.this, Appoinment.class);
+            EditText etPhone = findViewById(R.id.etPhone);
+            String Phone = etPhone.getText().toString();
+            intent.putExtra("Phone", Phone);
+            startActivity(intent);
         }
     };
     public void GoLog(View V) {
