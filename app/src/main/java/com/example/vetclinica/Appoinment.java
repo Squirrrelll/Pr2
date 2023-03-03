38e64b7d53f34911666eba4f2fb1e13b98322b64
Polina <poly.gera777@gmail.com>
2023-03-03 15:07:45 +0300

    Part 6

--- app/src/main/java/com/example/vetclinica/Appoinment.java
+++ app/src/main/java/com/example/vetclinica/Appoinment.java
@@ -8,16 +8,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Appoinment extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinment);
 
+        Bundle arguments = getIntent().getExtras();
+        String Phone = arguments.get("phone").toString();
+        TextView tvPhone = (TextView) findViewById(R.id.tvPhone);
+        tvPhone.setText(Phone);
+
         Button btnBack = (Button) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(ClickBack);
+
     }
 
     View.OnClickListener ClickBack = new View.OnClickListener() {
--- app/src/main/java/com/example/vetclinica/MainActivity.java
+++ app/src/main/java/com/example/vetclinica/MainActivity.java
@@ -4,37 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.net.IDN;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
-    TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Пункт 3
+        Button btnEnter = (Button) findViewById(R.id.btnreg); //кнопка "Войти"
+        TextView textView = (TextView) findViewById(R.id.textView);
+        EditText etPhone = (EditText) findViewById(R.id.etPhone); //ввод номера телефона
+        EditText etPassword = (EditText) findViewById(R.id.editTextTextPassword); //ввод пароля
 
-        Button btnEnter = (Button) findViewById(R.id.btnreg);
-        btnEnter.setOnClickListener(ClickLog);
+        btnEnter.setText(R.string.registrarte);
+        etPhone.setHint(R.string.phone);
+        etPassword.setHint(R.string.password);
 
+        //Пункт 6
         textView = (TextView) findViewById(R.id.fb1);
         Intent intent = getIntent();
         String name = intent.getStringExtra("namePet");
         textView.setText(name);
+
+        //Пункт 4
+        btnEnter.setOnClickListener(ClickLog);
+
+
+        //Пункт 5
     }
     View.OnClickListener ClickLog = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Log.i(TAG, "Программно");
+            // создание объекта Intent для запуска Appointment
             Intent intent = new Intent(MainActivity.this, Appoinment.class);
-            EditText etPhone = findViewById(R.id.etPhone);
+            EditText etPhone = (EditText) findViewById(R.id.etPhone); //ввод номера телефона
             String Phone = etPhone.getText().toString();
-            intent.putExtra("Phone", Phone);
+            // передача объекта с ключом "phone" и значением строки Phone
+            intent.putExtra("phone", Phone);
+            // запуск Appointment
             startActivity(intent);
         }
     };
